9521f5aea2ec4866d54e6b4b810534ded449c927
Ranjana1996 <ranjana.seshadri@gmail.com>
2017-12-07 19:39:18 +0530

    without inheritance

--- observable.cpp
+++ observable.cpp
@@ -1,15 +1,15 @@
 #include "observer.hpp"
 #include "observable.hpp"
 //#include "mySocket.hpp"
-#include "Subject.hpp"
+//#include "Subject.hpp"
 
 using namespace std;
 
 
- observable (mySocket &socket_in):socket(socket_in){};
+ observable (mySocket *socket_in):socket(socket_in){};
 	 
 
-observable (mySocket &socket_in,string path_in)
+observable (mySocket *socket_in,string path_in)
 	 { 
 		socket=socket_in; 
 		path=path_in;
@@ -66,6 +66,32 @@ void observable::sendResponse()
 states observable::getState()
 {
 	return state;
+}
+void observable::Attach (observer* o) 
+{ 
+ _observers.push_back(o); 
+} 
+
+void observable::Detach (observer* o) 
+{ 
+ int count = _observers.size(); 
+ int i; 
+
+ for (i = 0; i < count; i++) { 
+   if(_observers[i] == o) 
+   break; 
+ } 
+ if(i < count) 
+  _observers.erase(_observers.begin() + i); 
+
+} 
+
+void observable::Notify () 
+{ 
+ int count = _observers.size(); 
+
+ for (int i = 0; i < count; i++) 
+   (_observers[i])->update(this); 
 }
 
 
--- observable.hpp
+++ observable.hpp
@@ -1,24 +1,38 @@
 #ifndef OBSERVABLE_H
 #define OBSERVABLE_H
-#include "Subject.hpp"
+//#include "Subject.hpp"
 #include "states.hpp"
 #include "mySocket.hpp"
 #include <iostream>
 #include <fstream>
 
 
-class observable : public Subject
+//#include "socket.cpp"
+//#include <pthread.h>
+#include <unistd.h>
+//#include "rough.cpp" 
+#include <iostream>
+#include <string>
+//#include <queue>
+//#include <list>
+#include <cstdlib>
+#include <vector>
+#include "observer.hpp"
+
+
+class observable //: public Subject
 {
 private:
-	mySocket &socket;
+	mySocket socket;
 	string text;
     	string path;
 	states state=notRead;
+	vector< observer*> _observers;
 
 
 public: 
-	observable(mySocket &socket_in);
-    observable(mySocket &socket_in, string path_in);
+	observable(mySocket *socket_in);
+    observable(mySocket *socket_in, string path_in);
 	virtual ~observable(); 
 
 	//void readInput(string path);
@@ -26,6 +40,9 @@ public:
 	string getText();
 	states getState();
 	mySocket getSocket();
+	 void Attach(observer*); 
+ 	void Detach(observer*); 
+ 	 void Notify();  
 
     //void setPath();
     string getPath();
