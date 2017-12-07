b1ac871ad8f52f617cd956e4fd557adac49248a9
om <omiyorulz@gmail.com>
2017-12-07 11:20:21 +0530

    added header

--- observable.hpp
+++ observable.hpp
@@ -14,8 +14,6 @@ public:
     observable(mySocket * socket_in, string path);
 	virtual ~observable(); 
 
-	void addObserver(observer* );
-	void removeObserver(observer*);
 	void readInput();
 	string getText();
 	mySocket getSocket();
